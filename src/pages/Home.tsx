5fa27a06c3293e11698a80d7c7d464a846407942
Teemu Helenius <heleniusteemu@gmail.com>
2021-01-26 09:29:16 +0200

    init

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import ResponsiveDrawer from "../components/ResponsiveDrawer"
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <ResponsiveDrawer />
+    </div>
+  )
+}
