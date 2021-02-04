0936dbda8bf1c98f6457a07e8becab67ed47d077
Teemu Helenius <heleniusteemu@gmail.com>
2021-02-04 18:38:04 +0200

    Removed search from app bar and added search to courses page

---
+++ src/layouts/AppBar.tsx
@@ -0,0 +1,58 @@
+import React from "react"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
+import IconButton from "@material-ui/core/IconButton"
+import Typography from "@material-ui/core/Typography"
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles"
+import MenuIcon from "@material-ui/icons/Menu"
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up("sm")]: {
+        display: "none",
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up("sm")]: {
+        width: (drawerWidth) => `calc(100% - ${drawerWidth}px)`,
+        marginLeft: (drawerWidth) => drawerWidth,
+      },
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+)
+
+type AppBarProps = {
+  handleDrawerToggle: () => void
+  drawerWidth: number
+}
+
+export default ({
+  handleDrawerToggle,
+  drawerWidth,
+}: AppBarProps): JSX.Element => {
+  const classes = useStyles(drawerWidth)
+
+  return (
+    <AppBar className={classes.appBar}>
+      <Toolbar>
+        <IconButton
+          onClick={handleDrawerToggle}
+          className={classes.menuButton}
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography className={classes.title} variant="h6" noWrap>
+          Course-O-Rater
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
--- src/pages/Courses.tsx
+++ src/pages/Courses.tsx
@@ -1,10 +1,21 @@
 import React from "react"
 import List from "@material-ui/core/List"
+import { Grid, TextField } from "@material-ui/core"
 import CourseListItemLink from "../components/CourseListItemLink"
 
 export default (): JSX.Element => {
   return (
     <>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <TextField
+            id="search-course-text-field"
+            label="Search course"
+            variant="outlined"
+            fullWidth
+          />
+        </Grid>
+      </Grid>
       <List aria-label="List of courses">
         <CourseListItemLink
           courseName="Course name"
