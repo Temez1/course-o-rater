4484cbf0c21efe243e8d97180e64320847a34ebb
Teemu Helenius <heleniusteemu@gmail.com>
2021-03-18 11:43:23 +0200

    Init Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,22 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
+import firebase from "firebase/app"
+import "firebase/analytics"
 import App from "./App"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCev7G4HPURCMS4EYG0PGryqGgijOKlm7w",
+  authDomain: "course-o-rater.firebaseapp.com",
+  projectId: "course-o-rater",
+  storageBucket: "course-o-rater.appspot.com",
+  messagingSenderId: "713909971040",
+  appId: "1:713909971040:web:6f6fd3cc74d6ccb16ebb05",
+  measurementId: "G-7QTMH3BT7Z",
+}
+
+firebase.initializeApp(firebaseConfig)
+firebase.analytics()
 
 ReactDOM.render(
   <React.StrictMode>
