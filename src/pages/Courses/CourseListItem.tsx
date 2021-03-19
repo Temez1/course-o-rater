13fd6d12f69b82005dff5130cbfb84a84fc6e8b3
Teemu Helenius <heleniusteemu@gmail.com>
2021-03-19 13:08:30 +0200

    Fetch courses & calculate avg rating

--- src/pages/Courses/CourseListItem.tsx
+++ src/pages/Courses/CourseListItem.tsx
@@ -6,13 +6,15 @@ import {
   Link as RouterLink,
   LinkProps as RouterLinkProps,
 } from "react-router-dom"
+import { Typography } from "@material-ui/core"
 
 interface CourseListItemProps {
+  course: Course
   to: string
 }
 
 export default (props: CourseListItemProps): JSX.Element => {
-  const { to } = props
+  const { to, course } = props
 
   const renderLink = React.useMemo(
     () =>
@@ -23,12 +25,30 @@ export default (props: CourseListItemProps): JSX.Element => {
     [to]
   )
 
+  let totalCourseRatingsAverage
+
+  if (course.ratings) {
+    totalCourseRatingsAverage =
+      course.ratings.reduce(
+        (accumulator, courseRating) => accumulator + courseRating.totalRating,
+        0
+      ) / course.ratings.length
+  }
+
   return (
     <li>
       <ListItem button component={renderLink}>
-        <ListItemText primary="Course name" />
-        <ListItemText primary="Course code" />
-        <Rating readOnly precision={0.5} defaultValue={3.3} />
+        <ListItemText primary={course.name} />
+        <ListItemText primary={course.code} />
+        {totalCourseRatingsAverage ? (
+          <Rating
+            readOnly
+            precision={0.5}
+            defaultValue={totalCourseRatingsAverage}
+          />
+        ) : (
+          <Typography>No ratings yet!</Typography>
+        )}
       </ListItem>
     </li>
   )
--- src/pages/Courses/index.tsx
+++ src/pages/Courses/index.tsx
@@ -1,11 +1,46 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { List } from "@material-ui/core"
+import "firebase/firestore"
+import { useFirestore } from "reactfire"
 import CourseListItem from "./CourseListItem"
 
 export default (): JSX.Element => {
+  const firestore = useFirestore()
+
+  const [courses, setCourses] = useState<Courses | null>(null)
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const coursesQuery = await firestore.collection("courses").get()
+
+        if (coursesQuery.empty) {
+          // eslint-disable-next-line @typescript-eslint/no-throw-literal
+          throw "Courses not found"
+        }
+        setCourses(
+          coursesQuery.docs.map((doc) => {
+            return { id: doc.id, ...doc.data() } as Course
+          })
+        )
+      } catch (err) {
+        alert(err)
+      }
+    }
+
+    fetchData()
+  }, [firestore])
+
+  console.log(courses)
   return (
     <List aria-label="List of courses">
-      <CourseListItem to="course/courseID_Or_Name" />
+      {courses?.map((course) => (
+        <CourseListItem
+          key={course.id}
+          to="course/courseID_Or_Name"
+          course={course}
+        />
+      ))}
     </List>
   )
 }
