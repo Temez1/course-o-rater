8e20f6886696f7be398798685fc98a7d41712dcb
siikanen <santeri.siiranen@gmail.com>
2021-03-30 11:37:45 +0300

    Add google analytics for router page views

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from "react"
-import { useRoutes } from "react-router-dom"
+import React, { useEffect } from "react"
+import { useRoutes, useLocation } from "react-router-dom"
 import useMediaQuery from "@material-ui/core/useMediaQuery"
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles"
 import { CssBaseline } from "@material-ui/core"
 import lightBlue from "@material-ui/core/colors/lightBlue"
-import { useUser } from "reactfire"
+import { useUser, useAnalytics } from "reactfire"
+import "firebase/analytics"
 
 import routes from "./routes"
+
+const PageViewLogger = () => {
+  const analytics = useAnalytics()
+  const loc: Location = (useLocation() as unknown) as Location
+
+  useEffect(() => {
+    analytics.logEvent("page-view", { path_name: loc.pathname })
+  }, [loc.pathname, analytics])
+
+  return null
+}
 
 export default (): JSX.Element => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)", {
@@ -41,6 +53,7 @@ export default (): JSX.Element => {
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <CssBaseline />
       {routing}
+      <PageViewLogger />
     </ThemeProvider>
   )
 }
